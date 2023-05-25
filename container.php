4fbbd7bb3b6195e858f6a4ff07fc001b123e992e
xcoderk <92213896+xcoderk@users.noreply.github.com>
2023-05-25 18:04:42 +0000

    Add files via upload

---
+++ container.php
@@ -0,0 +1,12 @@
+</head>
+<body class="">
+<nav role="navigation" class="navbar navbar-dark navbar-static-top bg-primary bg-gradient">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="https://www.sourcecodester.com" class="navbar-brand">AYEKOO!! Food Ordering System</a>
+      </div>
+    </div>
+  </nav>
+	
+	<div class="container" style="min-height:500px;">
+	
